7874ca25ad5673aecf4ff64299eb86fb3a4a67ae
Evgeniy Kov <yrec_92@mail.ru>
2022-04-28 21:30:21 +0300

    Facade Pattern

---
+++ Main.java
@@ -0,0 +1,9 @@
+package PatternFacade;
+
+public class Main {
+    public static void main(String[] args) {
+        BinOps binOps = new BinOps();
+        System.out.println(binOps.mult("1", "10"));
+        System.out.println(binOps.sum("11", "111"));
+    }
+}
